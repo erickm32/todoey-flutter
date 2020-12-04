3e51f6aabf3f93d2616c30ebb2ff44aa656a24f9
Érick Moreira <erick@sementesrastreadas.com.br>
2020-12-03 21:15:50 -0300

    Minor tweak - making 'tasks' private

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,20 +1,26 @@
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 import 'package:todoey_flutter/models/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [Task(text: 'teste')];
+  List<Task> _tasks = [Task(text: 'teste')];
 
   int get tasksCount {
-    return tasks.length;
+    return _tasks.length;
+  }
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
   }
 
   void addTask(Task task) {
-    tasks.add(task);
+    _tasks.add(task);
     notifyListeners();
   }
 
   void removeTask(Task task) {
-    tasks.remove(task);
+    _tasks.remove(task);
     notifyListeners();
   }
 
